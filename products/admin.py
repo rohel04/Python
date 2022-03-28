93baf2c1ac3fd0c4e25a3e5a92cf393e961eb241
rohel04 <rohelshk@gmail.com>
2022-03-28 21:50:21 +0545

    Second commit

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,14 @@
 import imp
+from pyexpat import model
 from django.contrib import admin
 from products.models import Product,Offer
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Offer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','stock')
+    
+class OfferAdmin(admin.ModelAdmin):
+    list_display=('code','description','discount')
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Offer,OfferAdmin)
--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
+from itertools import product
 from django.shortcuts import render
 from django.http import HttpResponse as res
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return res("Welcome to first project")
+    # return res("Welcome to first project")
+    products=Product.objects.all()
+    return render(request,'index.html',{'products':products})
 
 def add(request):
     return res("Add products")
